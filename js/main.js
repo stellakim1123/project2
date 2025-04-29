8e15b4a698e3646063e71f542c7c7b4491b5b266
stellakim1123 <glamstellakim@gmail.com>
2025-04-29 18:09:10 +0900

    25.04.29.

--- js/main.js
+++ js/main.js
@@ -11,3 +11,18 @@ $(window).resize(function () {
         $(".b").show(); 
     }
 });
+
+$(document).ready(function(){
+    $(".prev").click(function(){
+        $(".slide li:last").prependTo(".slide");
+        $(".slide").css("margin-left", "-100%");
+        $(".slide").stop().animate({marginLeft:0}, 800);
+    });
+
+    $(".next").click(function(){
+        $(".slide").stop().animate({marginLeft: "-100%"}, 800, function(){
+            $(".slide li:first").appendTo(".slide");
+            $(".slide").css({marginLeft:0});
+        });
+    });
+});
